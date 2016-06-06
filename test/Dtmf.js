723fc7ee0105e06cdcaab1817663e49aa4e7ba99
Andrés Souto <kai670@gmail.com>
2016-06-06 13:43:15 +0200

    Fix indentation

--- test/Dtmf.js
+++ test/Dtmf.js
@@ -1,22 +1,22 @@
 import {Dtmf} from '../';
 
 describe('Dtmf', function() {
-  let dtmf;
+	let dtmf;
 
-  beforeEach(() => {
-    dtmf = new Dtmf();
-    expect(dtmf).to.be.instanceof(Dtmf);
-  });
+	beforeEach(() => {
+		dtmf = new Dtmf();
+		expect(dtmf).to.be.instanceof(Dtmf);
+	});
 
-  afterEach(() => {
-    dtmf.close();
-  });
+	afterEach(() => {
+		dtmf.close();
+	});
 
-  it('should play a DTMF during 200ms', (done) => {
-    dtmf.playDtmf('1');
-    setTimeout(() => {
-      dtmf.stop();
-      done();
-    },200);
-  });
+	it('should play a DTMF during 200ms', (done) => {
+		dtmf.playDtmf('1');
+		setTimeout(() => {
+			dtmf.stop();
+			done();
+		},200);
+	});
 });
